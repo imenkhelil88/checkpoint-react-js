d578ff5ffc70012f90cd3247bff76396f0b694ea
imenkhelil88 <imenkhelil88@gmail>
2023-05-05 11:31:49 +0200

    checkpoint react-js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Formulaire from './Component/Formulaire';
+
+function App() {
+  return (
+    <div className="App">
+    
+  <Formulaire/>
+     
+    </div>
+  );
+}
+
+export default App;
